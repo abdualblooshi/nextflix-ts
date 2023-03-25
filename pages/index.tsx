4db721ae06fe38680b33ad68843410b39e8dd0a6
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-03-25 15:32:58 +0400

    Changed H1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl">Nextflix: A netflix clone</h1>
+      <h1 className="text-4xl text-red-400">Nextflix: A netflix clone haha</h1>
     </>
   );
 }
