2a4f3296ca8461ed97c89b64706d771c6c1214da
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-03 15:35:40 +0400

    Globalized Message Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { RecoilRoot, useRecoilState } from "recoil";
+import type { AppProps } from "next/app";
+import Message from "@/components/message";
+import { isVisibleState, messageState, statusState } from "@/lib/message";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const AppWrapper = ({ Component, pageProps }: AppProps) => {
+  const [messageVisible, setMessageVisible] = useRecoilState(isVisibleState);
+  const [message, setMessage] = useRecoilState(messageState);
+  const [status, setStatus] = useRecoilState(statusState);
+
+  return (
+    <>
+      <Message
+        message={message}
+        setMessageVisible={setMessageVisible}
+        messageVisible={messageVisible}
+        status={status}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default function App(appProps: AppProps) {
+  return (
+    <RecoilRoot>
+      <AppWrapper {...appProps} />
+    </RecoilRoot>
+  );
 }
