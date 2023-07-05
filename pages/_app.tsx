253538074c7fbff3afae393f73ec6da0c7065b80
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-05 13:55:53 +0400

    Added loading functionality for future uses

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,45 @@
 import "@/styles/globals.css";
+import { useCallback, useEffect } from "react";
 import { RecoilRoot, useRecoilState } from "recoil";
 import type { AppProps } from "next/app";
 import Message from "@/components/message";
-import { isVisibleState, messageState, statusState } from "@/lib/message";
+import {
+  isVisibleState,
+  messageState,
+  statusState,
+  isLoadingState,
+} from "@/lib/message";
+import TV from "@/components/tv";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 const AppWrapper = ({ Component, pageProps }: AppProps) => {
   const [messageVisible, setMessageVisible] = useRecoilState(isVisibleState);
   const [message, setMessage] = useRecoilState(messageState);
   const [status, setStatus] = useRecoilState(statusState);
+  const [isLoading, setIsLoading] = useRecoilState(isLoadingState);
+
+  // handle page change loading
+  const router = useRouter();
+  const handleStart = useCallback(() => {
+    setIsLoading(true);
+  }, [setIsLoading]);
+
+  const handleComplete = useCallback(() => {
+    setIsLoading(false);
+  }, [setIsLoading]);
+
+  // show loading on page change
+  useEffect(() => {
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [handleStart, handleComplete, router.events]);
 
   return (
     <>
