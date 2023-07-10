266d20d2e7c207301cd3b58277e5c3f09a34fdee
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-10 13:59:18 +0400

    Removed phone authentication from nextauth as it is not needed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,64 +1,45 @@
-import NextAuth from "next-auth/next";
+import NextAuth, { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
-import prismadb from "../../../lib/prismadb";
+import prismadb from "@/libs/prismadb";
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     Credentials({
       id: "credentials",
       name: "Credentials",
       credentials: {
-        email: { label: "Email", type: "text" } || {
-          label: "Phone Number",
+        email: {
+          label: "Email",
           type: "text",
         },
-        password: { label: "Password", type: "password" },
+        password: {
+          label: "Password",
+          type: "password",
+        },
       },
       async authorize(credentials) {
-        // Check if the email and password are provided
         if (!credentials?.email || !credentials?.password) {
-          throw new Error(
-            "Please enter your email address/phone number and password"
-          );
+          throw new Error("Email and password required");
         }
 
-        // Check if its an email or a phone number
-        const isPhone = /^\+?[0-9]{1,3}[0-9]{9}$/.test(credentials?.email);
-        const isEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-          credentials?.email
-        );
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-        // Check if the email/phone number is valid
-        if (!isPhone && !isEmail) {
-          throw new Error("Please enter a valid email address/phone number");
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
         }
 
-        const user = isPhone
-          ? await prismadb.user.findFirst({
-              where: {
-                phone: credentials?.email,
-              },
-            })
-          : await prismadb.user.findUnique({
-              where: {
-                email: credentials.email,
-              },
-            });
-
-        // Check if the user exists
-        if (!user) {
-          throw new Error("Invalid login credentials");
-        }
-
-        // Compare the password with the hash stored in the database
-        const isValid = await compare(
+        const isCorrectPassword = await compare(
           credentials.password,
-          user?.hashedPassword as string
+          user.hashedPassword
         );
 
-        if (!isValid) {
-          throw new Error("Invalid login credentials");
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
         }
 
         return user;
@@ -69,11 +50,11 @@ export default NextAuth({
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
+  session: { strategy: "jwt" },
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.NEXTAUTH_JWT_SECRET,
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+export default NextAuth(authOptions);
