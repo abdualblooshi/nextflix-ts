d188c07791c25703f0bc162ece3ccaddb7d3329f
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-10 13:58:01 +0400

    Added a test signout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import { NextPageContext } from "next";
+import { signOut, getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  // print session for debugging
+  console.log(session);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
       <h1 className="text-4xl text-red-400">Nextflix: A netflix clone haha</h1>
+      <button
+        className="bg-gray-500 text-white px-10 py-4 rounded-lg"
+        onClick={() => signOut()}
+      />
     </>
   );
 }
