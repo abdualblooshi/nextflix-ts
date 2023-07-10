1190bcba69e96bbf3d9ce4ce834712cfdedb8bd7
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-10 13:58:37 +0400

    Implemented rate limiting per minute

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,24 @@
 import { compare } from "bcrypt";
-import prismadb from "../../lib/prismadb";
+import prismadb from "../../libs/prismadb";
 import { NextApiRequest, NextApiResponse } from "next";
-import { signIn } from "next-auth/react";
+import { get, set } from "lodash";
+
+const rateLimit = 3; // Number of allowed requests per minute
+const rateLimiter: { [ip: string]: number[] } = {};
+
+const rateLimiterMiddleware = (ip: string | string[]) => {
+  const now = Date.now();
+  const windowStart = now - 60 * 1000; // 1 minute ago
+
+  const requestTimestamps = get(rateLimiter, ip, []).filter(
+    (timestamp: number) => timestamp > windowStart
+  );
+  requestTimestamps.push(now);
+
+  set(rateLimiter, ip, requestTimestamps);
+
+  return requestTimestamps.length <= rateLimit;
+};
 
 // this is a custom error handler that will be used to handle errors thrown by the error in the url query by nextauth
 
@@ -15,6 +32,20 @@ export default async function handler(
     }
 
     const { email, password } = req.body;
+
+    const ip: string | string[] | undefined =
+      req.headers["x-real-ip"] || req.socket.remoteAddress || "";
+
+    const countdown = get(rateLimiter, ip, []).length;
+    // convert to seconds
+    const seconds = Math.floor(countdown / 1000);
+    const timeRemaining = 60 - seconds;
+
+    if (!rateLimiterMiddleware(ip)) {
+      return res.status(429).json({
+        error: `Too many requests, please try again later.`,
+      });
+    }
 
     if (!email && !password) {
       return res
@@ -36,13 +67,15 @@ export default async function handler(
 
     // Check if the email/phone number is valid
     if (!isPhone && !isEmail) {
-      throw new Error("Please enter a valid email address/phone number");
+      return res
+        .status(422)
+        .json({ error: "Please enter a valid email address/phone number" });
     }
 
     const user = isPhone
       ? await prismadb.user.findFirst({
           where: {
-            phone: email,
+            phone: email.replace(/\+/g, "").replace(/\s/g, ""),
           },
         })
       : await prismadb.user.findUnique({
@@ -60,22 +93,12 @@ export default async function handler(
       return res.status(422).json({ error: "Invalid login credentials" });
     }
 
-    const session = await signIn("credentials", {
-      redirect: false,
-      email: user?.email,
-      password,
-      callbackUrl: `/`,
-    });
-    if (!session) {
-      return res.status(422).json({ error: "Invalid login credentials" });
-    }
-
     return res.status(200).json({ user, message: "Login successful" });
   } catch (error) {
     return res.status(400).json({
-      error: `Something went wrong: ${error}`,
+      error: `${error}`,
       status: "error",
-      message: "Something went wrong",
+      message: "",
     });
   }
 }
