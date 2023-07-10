3d5b552de3dcea026f6101d7538ffb717a553871
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-10 22:39:49 +0400

    A menu that has logout button and select user

---
+++ components/AccountMenu.tsx
@@ -0,0 +1,45 @@
+import { signOut } from "next-auth/react";
+import React from "react";
+import Image from "next/image";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+interface AccountMenuProps {
+  visible?: boolean;
+}
+
+const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  const { data: currentUser } = useCurrentUser();
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="flex flex-col gap-3">
+        <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
+          <Image
+            className="w-8 rounded-md"
+            src="/images/default-blue.png"
+            alt="Profile"
+            width={500}
+            height={500}
+          />
+          <p className="text-white text-sm group-hover/item:underline">
+            {currentUser?.firstName}
+          </p>
+        </div>
+      </div>
+      <hr className="bg-gray-600 border-0 h-px my-4" />
+      <div
+        onClick={() => signOut()}
+        className="px-3 text-center text-white text-sm hover:underline"
+      >
+        Sign out of Netflix
+      </div>
+    </div>
+  );
+};
+
+export default AccountMenu;
