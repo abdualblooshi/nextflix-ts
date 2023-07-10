2b7ea1013ea03d6e3c8e27c518a5e4b105784e68
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-10 09:58:00 +0400

    Current User hook

---
+++ hooks/useCurrentUser.ts
@@ -0,0 +1,15 @@
+import useSWR from "swr";
+import { fetcher } from "@/lib/fetcher";
+
+const useCurrentUser = () => {
+  const { data, error, isLoading, mutate } = useSWR("/api/session", fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export default useCurrentUser;
