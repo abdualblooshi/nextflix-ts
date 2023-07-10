e89e15711bc127f03c65ba653b52864a354884aa
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-10 13:55:55 +0400

    Changed lib to libs

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import { fetcher } from "@/lib/fetcher";
+import { fetcher } from "@/libs/fetcher";
 
 const useCurrentUser = () => {
   const { data, error, isLoading, mutate } = useSWR("/api/session", fetcher);
