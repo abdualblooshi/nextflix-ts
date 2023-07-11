7f667ee2099dd0ff7451cc5f2d147fe53775c5f9
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-11 16:53:35 +0400

    Added some extra hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
+import React from "react";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
-import useCurrentUser from "@/hooks/useCurrentUser";
+
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
+import InfoModal from "@/components/InfoModal";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  // print session for debugging
-  console.log(session);
 
   if (!session) {
     return {
@@ -24,13 +27,22 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export default function Home() {
-  const { data: user } = useCurrentUser();
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore();
+
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
-      <MovieList data={user?.favoriteMovies} title="My Favorites" />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   );
-}
+};
+
+export default Home;
