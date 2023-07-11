dc74307cd2a11b085af67ed398fd4d2cc3729b44
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-11 17:56:07 +0400

    Set volume to 50 and added page title

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-import { useRouter } from 'next/router';
-import useMovie from '@/hooks/useMovie';
+import React, { useEffect } from "react";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+import useMovie from "@/hooks/useMovie";
 
 const Watch = () => {
   const router = useRouter();
   const { movieId } = router.query;
 
   const { data } = useMovie(movieId as string);
-  
+
+  // set video volume to 0.5
+  useEffect(() => {
+    const video = document.querySelector("video");
+    if (video) {
+      video.volume = 0.5;
+    }
+  }, []);
+
+  // set page title to Playing: {movie title}
+  useEffect(() => {
+    document.title = `Playing: ${data?.title}`;
+  }, [data]);
+
   return (
     <div className="h-screen w-screen bg-black">
       <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
-        <ArrowLeftIcon onClick={() => router.push('/')} className="w-4 md:w-10 text-white cursor-pointer hover:opacity-80 transition" />
+        <ArrowLeftIcon
+          onClick={() => router.push("/")}
+          className="w-4 md:w-10 text-white cursor-pointer hover:opacity-80 transition"
+        />
         <p className="text-white text-1xl md:text-3xl font-bold">
           <span className="font-light">Watching:</span> {data?.title}
         </p>
       </nav>
-      <video className="h-full w-full" autoPlay controls src={data?.videoUrl}></video>
+      <video
+        className="h-full w-full"
+        autoPlay
+        controls
+        src={data?.videoUrl}
+      ></video>
     </div>
-  )
-}
+  );
+};
 
 export default Watch;
