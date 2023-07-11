804530719b6afb115813ba0d3342ff07bd4b08cd
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-11 16:42:20 +0400

    Get all movies API

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -11,10 +11,13 @@ export default async function handler(
       return res.status(405).end();
     }
 
+    // wait for user to be authenticated
     await serverAuth(req, res);
 
+    // get all movies
     const movies = await prismadb.movie.findMany();
 
+    // return movies
     return res.status(200).json(movies);
   } catch (error) {
     console.log({ error });
