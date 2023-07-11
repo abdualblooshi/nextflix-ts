aae4dd04ab0d68344d6c476613c3b5f0d2d9fb10
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-07-11 14:28:54 +0400

    Play Something API functionality

---
+++ pages/api/random.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/libs/prismadb";
+import serverAuth from "@/libs/serverAuth";
+
+// What this does is that it will get a random movie from the database and return it to the client
+
+// more like what netflix does when you click on the "play something" button and it plays a random movie for you to watch
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method !== "GET") {
+      return res.status(405).end();
+    }
+
+    await serverAuth(req, res);
+
+    const moviesCount = await prismadb.movie.count();
+    const randomIndex = Math.floor(Math.random() * moviesCount);
+
+    const randomMovies = await prismadb.movie.findMany({
+      take: 1,
+      skip: randomIndex,
+    });
+
+    return res.status(200).json(randomMovies[0]);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).end();
+  }
+}
